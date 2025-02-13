c6ee5d071fa31dbfc2b2e53055bfde532ceb3de0
shivamjai08 <shijai2580@gmail.com>
2025-02-13 20:50:01 +0530

    First Commit

---
+++ src/Basicprogramme/com/conditional_statement.java
@@ -0,0 +1,59 @@
+package Basicprogramme.com;
+
+public class conditional_statement {
+    public static void main(String[] args) {
+        //logical operations
+        System.out.println("This is && operation perform in Logical opeartion");
+//        boolean a=true;
+//        boolean b=false;
+//        if (a&&b){
+//            System.out.println("y");
+//        }
+//        else{
+//            System.out.println("n");
+
+       // }
+//        boolean a=true;
+//        boolean b= true ;
+//        if (a&&b){
+//            System.out.println("y");
+//        }
+//        else {
+//            System.out.println("n");
+//        }
+//        System.out.println("OR operation in logical operation ||");
+//boolean a=true;
+//boolean b=false;
+//boolean b=true;
+//if (a||b){
+//    System.out.println("y");
+//
+//}
+//else {
+//    System.out.println("n");
+//}
+        //In and we can see that any one value is true so the output was generate is also true ;
+
+//
+//        System.out.println("NOT operation it is also known as a inverse operation");
+//        boolean a=true;
+//        boolean b =false;
+//        System.out.println("NOT logical operator");
+//        System.out.println("NOT(A) is :");
+//        System.out.println(!a);
+//        System.out.println("NOT(b) is:");
+//        System.out.println(!b);
+
+        //Simple conditional statement:
+//        System.out.println("Simple Conditional Statement");
+//        int age=18;
+//        if (age>18){
+//            System.out.println("You Can Drive ");
+//
+//        }
+//        else{
+//            System.out.println("You can't Drive ");
+//        }
+    }
+
+}
