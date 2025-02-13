bfbcf50ea3850aef76a84ffd52caeaab4472b005
Shivam Jaiswal <shijai2580@gmail.com>
2025-02-13 23:19:19 +0530

    Update string_methods_in_java.java

--- src/Basicprogramme/com/string_methods_in_java.java
+++ src/Basicprogramme/com/string_methods_in_java.java
@@ -5,10 +5,13 @@ public class string_methods_in_java {
         String name = "Harry";
         int value = name.length();
         System.out.println(value);
+        //here we can get a value of our string
         String lstring = name.toLowerCase();
         System.out.println(lstring);
+        // In This part stringchange present uppercase  alphabets changed into lower case  
         String ustring = name.toUpperCase();
         System.out.println(ustring);
+        //In  this part of string change present lower alphabet into upper case 
         String nonTrimmedString = "   Harry       ";
         System.out.println(nonTrimmedString);
         String TrimmedString = nonTrimmedString.trim();
