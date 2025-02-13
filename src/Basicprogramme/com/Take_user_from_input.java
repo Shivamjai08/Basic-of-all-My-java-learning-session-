ef29ab089a5400990d2adeff059f9a314297d505
shivamjai08 <shijai2580@gmail.com>
2025-02-13 21:11:25 +0530

    second commit

---
+++ src/Basicprogramme/com/Take_user_from_input.java
@@ -0,0 +1,11 @@
+package Basicprogramme.com;
+import java.util.Scanner;
+public class Take_user_from_input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Write The Number :");
+ int a= sc.nextInt();
+        System.out.println(a>9);
+    }
+
+}
