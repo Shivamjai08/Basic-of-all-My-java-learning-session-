d42cacd04ccd777231348d1108706d860921fed2
shivamjai08 <shijai2580@gmail.com>
2025-02-13 21:21:35 +0530

    Fourth commit

---
+++ src/Basicprogramme/com/Get_user_from_string.java
@@ -0,0 +1,12 @@
+package Basicprogramme.com;
+import java.util.Scanner;
+
+public class Get_user_from_string {
+    public static void main(String[] args) {
+        String name = "harry";
+        System.out.println(name);
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
