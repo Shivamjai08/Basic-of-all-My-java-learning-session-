f25e296f6e4293a46ae1ac78f42f2cadafbda216
shivamjai08 <shijai2580@gmail.com>
2025-02-13 21:14:28 +0530

    third commit

---
+++ src/Basicprogramme/com/Basic_structure_of_java.java
@@ -0,0 +1,7 @@
+package Basicprogramme.com;
+
+public class Basic_structure_of_java {
+    public static void main(String[] args) {
+        System.out.println("Hello This Is My First Programme ");
+    }
+}
