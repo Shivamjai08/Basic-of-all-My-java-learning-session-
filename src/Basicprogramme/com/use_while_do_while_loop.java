663dc2842c99d58eb4c9ec41a0ffd3ea60f207d8
shivamjai08 <shijai2580@gmail.com>
2025-02-16 00:43:42 +0530

    Eight-commit

---
+++ src/Basicprogramme/com/use_while_do_while_loop.java
@@ -0,0 +1,119 @@
+package Basicprogramme.com;
+public class use_while_do_while_loop {
+    public static void main(String[] args) {
+        // using  While loop to print  numbers
+/*
+        int i = 0;
+
+        while (i <= 10) {
+            System.out.println(i++);
+            if (i == 4) {
+                System.out.println("end loop");
+                break; // in while loop we can use break statement
+            }
+            System.out.println("Here finish my loop ");
+*/
+            // infinite loop
+/*
+       while (true) {
+           System.out.println("good night ");
+       }
+
+
+
+ */
+            // Do-While loop in java programme.
+/*
+        int a=0;
+        do {
+            System.out.println(a++);
+        }while (a<5);
+*/
+
+/*
+    int a=0;
+    do {
+        System.out.println(a++);
+        if (a==3){
+            System.out.println("Ending the loop");
+            continue; // break statement is used to break a loop statement in do-while loop
+        }      //break;       // continue statement is used to continue a loop statement in do -while loop
+    }
+    while(a<101);
+        System.out.println("Loops ends here ");
+
+
+
+ */
+            // For Loop  Basic Programme:
+            // System.out.println(" This is for loop programme ");
+      /*   for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+*/
+            // question for quick practice, for loop
+       /* System.out.println("To print an odd number ");
+        for (int i=0; i<=5;i++){
+            System.out.println(2*i+1);
+        }
+
+        */
+
+/*
+         System.out.println("TO print an even number ");
+         int n=5;
+        for (int i=0; i<n; i++){
+            System.out.println(2*i);
+        }
+        */
+            // decrementing for loop
+        /*
+        System.out.println("Decremental of for loop");
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for (int i=100;i>=n; i--){
+            System.out.println(i);
+        }*/
+        /*System.out.println("Print reverse order natural number ");
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for( int i=1000; i>=n;i--) {
+            System.out.println(i);
+        }
+
+         */
+
+            // Break And Continue loop :
+//  'break syntax' break the loop statement:
+      /*
+        for (int i=0;i<5;i++)
+        {
+            System.out.println(i);
+            System.out.println("Java is Great ");
+            if (i==3){
+                System.out.println("Ending The Loop");
+                break;
+            }
+        }
+
+
+       */
+            // continue Statement
+       // for (int i=0; i<=100; i++){
+         //   if (i == 10) {
+           //     System.out.println( "loop is continue here");
+             //   continue;
+            //}
+            //System.out.println(i);
+            //System.out.println("java is greater");
+        //}
+
+
+
+
+
+        }
+    }
+
